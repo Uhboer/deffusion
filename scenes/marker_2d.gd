ff596aa12c1149c65858e337d64a501e5164a4e4
Uhboer <grishuk_oleg@gmail.com>
2024-09-05 20:35:56 +0300

    shotgun tweak

--- scenes/marker_2d.gd
+++ scenes/marker_2d.gd
@@ -1,21 +1,26 @@
 extends Node2D
 
-@export var bullet : PackedScene
+@export var bullet_scene : PackedScene
 
 @onready var character = $".."
 @onready var anim = $wearpon
+
+var shooted = false
 
 func _process(delta):
 	look_at(get_global_mouse_position())
 	
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
-		await get_tree().create_timer(2).timeout
+		
 	
 
 func shoot():
-	var b = bullet.instantiate()
-	add_child(b)
-	b.transform = $Node/Marker2D.global_transform
-	anim.play("ShootgunPump")
-	
+	if  shooted == false:
+		shooted = true
+		var b = bullet_scene.instantiate()
+		add_child(b)
+		b.transform = $Node/Marker2D.global_transform
+		anim.play("ShootgunPump")
+		await get_tree().create_timer(1.0).timeout
+		shooted = false
